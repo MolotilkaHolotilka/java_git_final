21c2dc3706ec034ec46fc81b3e38b802553fad19
MolotilkaHolotilka <ilya@rybushkin.ru>
2025-12-14 15:31:02 +0300

    Добавил сохранялку

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,31 @@
 package org.example;
 import java.util.*;
 import java.util.random.RandomGenerator;
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+import java.io.*;//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+    private static Integer readRecord(String filename) {
+        try {
+            File f = new File(filename);
+            if (!f.exists()) return null;
+
+            try (Scanner sc = new Scanner(f)) {
+                if (!sc.hasNextInt()) return null;
+                return sc.nextInt();
+            }
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    private static void writeRecord(String filename, int value) {
+        try (PrintWriter out = new PrintWriter(filename)) {
+            out.println(value);
+        } catch (Exception e) {
+            System.out.println("Не удалось записать рекорд: " + e.getMessage());
+        }
+    }
+
     public static void main(String[] args) {
         Boolean playAgain = true;
         while(playAgain == true){
@@ -28,10 +50,16 @@ public class Main {
         }
         System.out.println("Ура, число угадано!");
         System.out.println("Число попыток: " + countOfAttempts);
+        String filename = "record.txt";
+        Integer record = readRecord(filename);
+        if(record == null || countOfAttempts < record){
+            writeRecord(filename, countOfAttempts);
+        }
         System.out.println("Сыграть ещё раз? (y/n) ");
         in.nextLine(); // очистка после nextInt
+            Integer recordNew = readRecord(filename);
+            System.out.println("Твой рекорд:" + recordNew);
         String choice = in.nextLine();
-
         playAgain = choice.trim().equalsIgnoreCase("y");
 
     }}
